1ae8690ba756f2b7b0bd3caa637947a8a004e153
chengweixuan <exweixuan@gmail.com>
2021-07-31 02:00:49 +0800

    added swagger ui

--- src/main/java/com/weixuan/shorturl/ShorturlApplication.java
+++ src/main/java/com/weixuan/shorturl/ShorturlApplication.java
@@ -2,8 +2,10 @@ package com.weixuan.shorturl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ShorturlApplication {
 
 	public static void main(String[] args) {
